5b20b86ff997509c3a511b1ef6ebab9373b33767
subramanyam <subramanyam.kmk03@gmail.com>
2018-10-30 21:15:28 +0530

    added cost function class

---
+++ src/costs/costs.py
@@ -0,0 +1,17 @@
+class CostFunction():
+	def __init__(self):
+
+	def cost_r(self, rm):
+		return 0
+
+	def cost_s(self, sm):
+		return 0
+
+	def compute_unary_costs(self):
+		return 0
+
+	def compute_pairwise_costs(self):
+		return 0
+
+	def get_features(self):
+		return []
